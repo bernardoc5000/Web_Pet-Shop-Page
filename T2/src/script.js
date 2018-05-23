711dd95bf66bc88f5206e5c2d2ec42d450b7003d
eduardoscsouza@gmail.com <eduardoscsouza@gmail.com>
2018-05-23 18:24:51 -0300

    Fixing big

--- T2/src/script.js
+++ T2/src/script.js
@@ -1,13 +1,27 @@
-function login() {
-	let user = $("#User").val();
-	let psw = $("#Password").val();
+function login_out(in_out) {
+	if (in_out == 0){
+		let user = $("#User").val();
+		let psw = $("#Password").val();
 
-	if(user.localeCompare("user") == 0 && psw.localeCompare("user") == 0){
-		$("body").load("src/usuario_general.html")
-		$("#Content").load("src/usuario_compras.html")
+		if (user.localeCompare("user") == 0 && psw.localeCompare("user") == 0){
+			$("body").load("src/usuario_general.html")
+			$("#Content").load("src/usuario_compras.html")
+		}
+		else{
+			$("body").load("src/usuario_general.html")
+			$("#Content").load("src/usuario_compras.html")
+		}
 	}
 	else{
-		$("body").load("src/usuario_general.html")
-		$("#Content").load("src/usuario_compras.html")
+		$("body").load("index.html")
+	}
+}
+
+function changeUserPage(page){
+	if (page == 0){
+
+	}
+	else if (page == 1){
+
 	}
 }
