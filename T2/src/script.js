934d3bab036832e6f4080c520484538b0a8dbe9e
Bernardo <cunhabernardobc@gmail.com>
2018-05-30 17:20:26 -0300

    Added some of the admin pages

--- T2/src/script.js
+++ T2/src/script.js
@@ -4,11 +4,17 @@ function login_out(in_out) {
 		let psw = $("#Password").val();
 
 		if (user.localeCompare("user") == 0 && psw.localeCompare("user") == 0){
+			$("#Top").load("src/logged_top.html");
 			$("body").load("src/usuario_general.html");
 			$("#Content").load("src/usuario_compras.html");
 		}
+		else if (user.localeCompare("admin") == 0 && psw.localeCompare("admin") == 0){
+			$("#Top").load("src/logged_top.html");
+			$("#Menu").load("src/admin_menu.html");
+			$("#Content").load("src/admin_lucros.html");
+		}
 		else{
-			$("#Top").load("src/usuario_top.html");
+			$("#Top").load("src/logged_top.html");
 			$("#Menu").load("src/usuario_menu.html");
 			$("#Content").load("src/usuario_compras.html");
 		}
@@ -33,5 +39,20 @@ function changeUserPage(page){
 	}
 	else if (page == 4){
 		$("#Content").load("src/usuario_carrinho.html");
+	}
+}
+
+function changeAdminPage(page){
+	if (page == 0){
+		$("#Content").load("src/admin_cadastro_cliente.html");
+	}
+	else if (page == 1){
+		$("#Content").load("src/admin_produtos_adicionar.html");
+	}
+	else if (page == 2){
+		$("#Content").load("src/admin_servicos_adicionar.html");
+	}
+	else if (page == 3){
+		$("#Content").load("src/admin_lucros.html");
 	}
 }
