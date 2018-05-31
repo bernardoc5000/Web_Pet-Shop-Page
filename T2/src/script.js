a2f866d7bd450591b36dd321ce11cbeebd6f46db
eduardoscsouza@gmail.com <eduardoscsouza@gmail.com>
2018-05-31 20:16:41 -0300

    Building admin

--- T2/src/script.js
+++ T2/src/script.js
@@ -11,7 +11,10 @@ function login_out(in_out) {
 		else if (user.localeCompare("admin") == 0 && psw.localeCompare("admin") == 0){
 			$("#Top").load("src/logged_top.html");
 			$("#Menu").load("src/admin_menu.html");
-			$("#Content").load("src/admin_lucros.html");
+			jQuery.ajaxSetup({async:false});
+			$("#Content").load("src/admin_cadastro.html");
+			$("#MainContent").load("src/admin_cadastro_cliente.html");
+			jQuery.ajaxSetup({async:true});
 		}
 	}
 	else{
@@ -42,10 +45,16 @@ function changeUserPage(page){
 
 function changeAdminPage(page){
 	if (page == 0){
-		$("#Content").load("src/admin_cadastro_cliente.html");
+		jQuery.ajaxSetup({async:false});
+		$("#Content").load("src/admin_cadastro.html");
+		$("#MainContent").load("src/admin_cadastro_cliente.html");
+		jQuery.ajaxSetup({async:true});
 	}
 	else if (page == 1){
-		$("#Content").load("src/admin_produtos_adicionar.html");
+		jQuery.ajaxSetup({async:false});
+		$("#Content").load("src/admin_produtos.html");
+		$("#MainContent").load("src/admin_produtos_adicionar.html");
+		jQuery.ajaxSetup({async:true});
 	}
 	else if (page == 2){
 		$("#Content").load("src/admin_servicos_adicionar.html");
@@ -61,5 +70,26 @@ function animaisSidebar(page){
 	}
 	else{
 		$("#MainContent").load("src/usuario_animais_cadastro.html");
+	}
+}
+
+function adminCadastroSidebar(page){
+	if (page == 0){
+		$("#MainContent").load("src/admin_cadastro_cliente.html");
+	}
+	else{
+		$("#MainContent").load("src/admin_cadastro_admin.html");
+	}
+}
+
+function adminProdutosSidebar(page){
+	if (page == 0){
+		$("#MainContent").load("src/admin_produtos_adicionar.html");
+	}
+	else if (page == 1){
+		$("#MainContent").load("src/admin_produtos_remover.html");
+	}
+	else{
+		$("#MainContent").load("src/admin_produtos_editar.html");
 	}
 }
