f4f3d66059609fd76d6b5a2f2e865252effe9205
eduardoscsouza@gmail.com <eduardoscsouza@gmail.com>
2018-06-01 13:41:33 -0300

    Start coding db

--- T2/src/script.js
+++ T2/src/script.js
@@ -1,74 +1,81 @@
-function login_out(in_out) {
-	if (in_out == 0){
-		let user = $("#User").val();
-		let psw = $("#Password").val();
+//Dexie.delete("petshop_database");
+var db = new Dexie("petshop_database");
+db.version(1).stores({
+	users: 'username, password, type'
+});
+db.users.put({username: "user", password: "user", type: 0});
+db.users.put({username: "admin", password: "admin", type: 1});
 
-		if (user.localeCompare("user") == 0 && psw.localeCompare("user") == 0){
-			$("#Top").load("src/logged_top.html");
-			$("#Menu").load("src/usuario_menu.html");
-			$("#Content").load("src/usuario_compras.html");
+async function login_out(in_out){
+	if (in_out === 0){
+		let user = await db.users.get($("#User").val());
+		if (user !== undefined && user['password'] === $("#Password").val()){
+			if (user['type'] === 0){
+				$("#Top").load("src/logged_top.html");
+				$("#Menu").load("src/usuario_menu.html");
+				$("#Content").load("src/usuario_compras.html");
+			}
+			else{
+				$("#Top").load("src/logged_top.html");
+				$("#Menu").load("src/admin_menu.html");
+				jQuery.ajaxSetup({async:false});
+				$("#Content").load("src/admin_cadastro.html");
+				$("#MainContent").load("src/admin_cadastro_cliente.html");
+				jQuery.ajaxSetup({async:true});
+			}
 		}
-		else if (user.localeCompare("admin") == 0 && psw.localeCompare("admin") == 0){
-			$("#Top").load("src/logged_top.html");
-			$("#Menu").load("src/admin_menu.html");
-			jQuery.ajaxSetup({async:false});
-			$("#Content").load("src/admin_cadastro.html");
-			$("#MainContent").load("src/admin_cadastro_cliente.html");
-			jQuery.ajaxSetup({async:true});
-		}
+		else alert("Usuário ou Senha inválidos");
 	}
-	else{
-		$("body").load("index.html");
-	}
+	else $("body").load("index.html");
 }
 
 function changeUserPage(page){
-	if (page == 0){
+	if (page === 0){
 		$("#Content").load("src/usuario_compras.html");
 	}
-	else if (page == 1){
+	else if (page === 1){
 		$("#Content").load("src/usuario_servicos.html");
 	}
-	else if (page == 2){
+	else if (page === 2){
 		$("#Content").load("src/usuario_cadastro.html");
 	}
-	else if (page == 3){
+	else if (page === 3){
 		jQuery.ajaxSetup({async:false});
 		$("#Content").load("src/usuario_animais.html");
 		$("#MainContent").load("src/usuario_animais_lista.html");
 		jQuery.ajaxSetup({async:true});
 	}
-	else if (page == 4){
+	else if (page === 4){
 		$("#Content").load("src/usuario_carrinho.html");
 	}
 }
 
 function changeAdminPage(page){
-	if (page == 0){
+	if (page === 0){
 		jQuery.ajaxSetup({async:false});
 		$("#Content").load("src/admin_cadastro.html");
 		$("#MainContent").load("src/admin_cadastro_cliente.html");
 		jQuery.ajaxSetup({async:true});
 	}
-	else if (page == 1){
+	else if (page === 1){
 		jQuery.ajaxSetup({async:false});
 		$("#Content").load("src/admin_produtos.html");
 		$("#MainContent").load("src/admin_produtos_adicionar.html");
 		jQuery.ajaxSetup({async:true});
 	}
-	else if (page == 2){
+	else if (page === 2){
 		jQuery.ajaxSetup({async:false});
 		$("#Content").load("src/admin_servicos.html");
 		$("#MainContent").load("src/admin_servicos_adicionar.html");
 		jQuery.ajaxSetup({async:true});
 	}
-	else if (page == 3){
+	else if (page === 3){
 		$("#Content").load("src/admin_lucros.html");
 	}
 }
 
 function animaisSidebar(page){
-	if (page == 0){
+	if (page === 0){
 		$("#MainContent").load("src/usuario_animais_lista.html");
 	}
 	else{
@@ -77,7 +84,7 @@ function animaisSidebar(page){
 }
 
 function adminCadastroSidebar(page){
-	if (page == 0){
+	if (page === 0){
 		$("#MainContent").load("src/admin_cadastro_cliente.html");
 	}
 	else{
@@ -86,10 +93,10 @@ function adminCadastroSidebar(page){
 }
 
 function adminProdutosSidebar(page){
-	if (page == 0){
+	if (page === 0){
 		$("#MainContent").load("src/admin_produtos_adicionar.html");
 	}
-	else if (page == 1){
+	else if (page === 1){
 		$("#MainContent").load("src/admin_produtos_remover.html");
 	}
 	else{
@@ -98,10 +105,10 @@ function adminProdutosSidebar(page){
 }
 
 function adminServicosSidebar(page){
-	if (page == 0){
+	if (page === 0){
 		$("#MainContent").load("src/admin_servicos_adicionar.html");
 	}
-	else if (page == 1){
+	else if (page === 1){
 		$("#MainContent").load("src/admin_servicos_remover.html");
 	}
 	else{
