0d12f5eb03345b249d1e941ed699fa268b0e7a74
eduardoscsouza@gmail.com <eduardoscsouza@gmail.com>
2018-06-21 16:33:59 -0300

    Add CouchDB

--- T3/server.js
+++ T3/server.js
@@ -1,8 +1,44 @@
+var nano = require('nano')("http://couchdb:couchdb@191.205.128.212:5984");
 var express = require('express');
+var delay = require('delay');
+
+var db = {};
+initializeDatabase();
+
+async function initializeDatabase(){
+	nano.db.list(function(err, body){
+		if (!err){
+			if(!body.includes("admins")) nano.db.create("admins", function(){ db.admins = nano.use("admins"); });
+			else db.admins = nano.use("admins");
+
+			if(!body.includes("clients")) nano.db.create("clients", function(){ db.clients = nano.use("clients"); });
+			else db.clients = nano.use("clients");
+		}
+	});
+
+	/*
+	let keepGoing;
+	do{
+		keepGoing = false;
+		for (let property in db) if (db.hasOwnProperty(property) && property === undefined) keepGoing = true;
+		await delay(500);
+    }while(keepGoing);
+    console.log(db.admins);
+	console.log(db.clients);
+	*/
+}
+//var db = nano.db.use("petshop");
+//db.insert({ crazy: true }, 0);
+//db.get(0, function(err, body) {
+//  if (!err)
+//    console.log(body);
+//});
+
+/*
 var page = express();
 page.use(express.static("./public"));
 page.listen(8000);
-
+*/
 /*
 var http = require('http')
 var path = require('path')
